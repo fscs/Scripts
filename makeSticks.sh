bdf6ee3e6ae723229e2714b6c0fa47862735e2a4
Philipp Rehs <P.Rehs@gmx.net>
2014-05-06 13:42:14 +0200

    Update makeSticks.sh
    Added beep when everything is ready

--- makeSticks.sh
+++ makeSticks.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-DD_PARAMETERS="bs=1M"
+DD_PARAMETERS="bs=4M"
 if [ $# != 2 ]; then
-	echo "$0 StickName Image"
-	echo "Es muss der Name der Sticks angegeben werden und der Pfad zum Image"
-	echo "Mögliche Namen:"
-	echo "$(ls /dev/disk/by-id | grep usb | grep -v part | cut -d'-' -f2 | cut -d'_' -f1 | uniq)"
+        echo "$0 StickName Image"
+        echo "Es muss der Name der Sticks angegeben werden und der Pfad zum Image"
+        echo "Mögliche Namen:"
+        echo "$(ls /dev/disk/by-id | grep usb | grep -v part | cut -d'-' -f2 | cut -d'_' -f1 | uniq)"
 else
-	for i in $(ls /dev/disk/by-id | grep $1 | grep -v part); do 
-		DEVICE=$(readlink -e /dev/disk/by-id/$i)
-		sudo dd if=$2 of=$DEVICE $DD_PARAMETERS &
-	done
-fi;
+        for i in $(ls /dev/disk/by-id | grep $1 | grep -v part); do 
+                DEVICE=$(readlink -e /dev/disk/by-id/$i)
+                dd if=$2 of=$DEVICE $DD_PARAMETERS &
+        done
+        echo "DD started"
+        while [ $(pidof dd | wc -l) -gt 0 ]
+        do
+                sleep 1
+        done
+        echo "Syncing"
+        sync
+        echo "Done";
+        beep -f 250  -l 300  -r 2
+fi
